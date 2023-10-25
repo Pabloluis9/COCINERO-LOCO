c4f50b4c4b22e9781ee1aeccd0bf4e5abc1e226f
Lorena Barrera <l.barreranicolas@gmail.com>
2023-10-25 18:57:32 +0200

    Total

--- JUEGO-COCINA/src/player.js
+++ JUEGO-COCINA/src/player.js
@@ -5,6 +5,8 @@ class Player {
         this.height = 100;
         this.x = 10;
         this.y = 400;
+        this.directionX = 0;
+        this.directionY = 0;
         
         this.element = document.createElement("img");
         this.element.src = "./assets/cocinero.png";
@@ -20,5 +22,47 @@ class Player {
         this.container.appendChild(this.element)
         this.element.style.position = "absolute";
     }
+
+    jump() {
+        if (!this.jumping) {
+          this.jumpingSpeed = 15;
+          this.jumping = true;
+        }
+      }
+
+      move() {
+        // Update player's car position based on directionX and directionY
+        this.left += this.directionX;
+        this.top += this.directionY;
+    
+        // Ensure the player's car stays within the game screen
+        // handles left hand side
+        if (this.left < 10) {
+          this.left = 10;
+        }
+    
+        // handles top side
+        if (this.top < 10) {
+          this.top = 10;
+        }
+    
+        // handles right hand side
+        if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
+          this.left = this.gameScreen.offsetWidth - this.width - 10;
+        }
+    
+        // handles bottom side
+        if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
+          this.top = this.gameScreen.offsetHeight - this.height - 10;
+        }
+    
+        // Update the player's car position on the screen
+        this.updatePosition();
+      }
+    
+      updatePosition() {
+        this.element.style.left = `${this.left}px`;
+        this.element.style.top = `${this.top}px`;
+      }
 
 }
