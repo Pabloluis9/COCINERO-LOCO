bcf429d96afb9d2a39fe6abb145d877ba0230fcd
Lorena Barrera <l.barreranicolas@gmail.com>
2023-10-28 09:12:14 +0200

    player move done

--- JUEGO-COCINA/src/game.js
+++ JUEGO-COCINA/src/game.js
@@ -4,5 +4,16 @@ class Game {
         this.background = new Background(this.container);
         this.player = new Player(this.container); 
         this.enemy = new Enemy(this.container);
+        this.intervalId = null;
     }
+
+    start() {
+        this.intervalId = setInterval(() => {
+          this.update();
+        }, 1000 / 36);
+      }
+      
+      update() {
+        this.player.move();
+      }
 }
